75a14e5bacc629db22314d05c11d41bfabf94c59
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2018-12-19 09:01:50 +0700

    add level 2

--- more/index.js
+++ more/index.js
@@ -1,29 +1,30 @@
-const names = ["Mr. Fahri",
-    "Mr. Abdul",
-    "Mrs. Josephhine",
-    "Mr. Joseph",
-    "Mr. Paul",
-    "Mrs. Paula",
-    "Mr. Hakim"
+const names = [
+  "Mr. Fahri",
+  "Mr. Abdul",
+  "Mrs. Josephhine",
+  "Mr. Joseph",
+  "Mr. Paul",
+  "Mrs. Paula",
+  "Mr. Hakim"
 ];
 
 const names1 = ["Mr. Fahri", "Mr. Abdul", "Mrs. Josephhine"];
 const names2 = ["Mr. Joseph", "Mr. Paul", "Mrs. Paula", "Mr. Hakim"];
 
 const names3 = [
-    "Mr. Fahri",
-    null,
-    undefined,
-    "Mr. Abdul",
-    24,
-    "Mrs. Josephhine",
-    "Mr. Joseph",
-    "Mr. Paul",
-    32,
-    "Mrs. Paula",
-    7,
-    22,
-    "Mr. Hakim"
+  "Mr. Fahri",
+  null,
+  undefined,
+  "Mr. Abdul",
+  24,
+  "Mrs. Josephhine",
+  "Mr. Joseph",
+  "Mr. Paul",
+  32,
+  "Mrs. Paula",
+  7,
+  22,
+  "Mr. Hakim"
 ];
 
 // Level 0
@@ -38,14 +39,21 @@ const printMr = names.filter(names => names.includes("Mr."));
 
 console.log(printMr);
 
-/*
 // Level 2
 console.log("Level 2 : Display names with gender");
-const printGender = names.filter((names) => if (names.includes("Mr.")) {
-    
-} else {
-    
-}); */
+names.forEach(function (gender) {
+    let text = ""
+    let i = 1;
+    if (gender.includes("Mr.")){
+        text += i+ gender + "(Male)";
+    } else{
+        text += i+ gender + "(Female)";
+    }
+
+    i++;
+
+    console.log(text);
+});
 
 /*
 // Level 3
@@ -54,7 +62,7 @@ const search_name = names.filter((names, name) => names.includes(name))
 console.log(search_name);
 */
 
-// Level 4 
+// Level 4
 console.log("Level 4 : Merger 2 Array");
 
 console.log(names1.concat(names2));
